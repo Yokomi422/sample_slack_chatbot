77e551b730b1406048eb28b2790393a13ea953b5
Rei Tsukada <rayzou1120@icloud.com>
2024-03-11 21:26:02 +0900

    プロンプトをgptに投げる処理

---
+++ gpt/gpt.go
@@ -0,0 +1,28 @@
+package gpt
+
+import (
+	"context"
+	"fmt"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func SendPrompt(client *openai.Client, prompt string) (string, error) {
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("ChatCompletion error: %v", err)
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
