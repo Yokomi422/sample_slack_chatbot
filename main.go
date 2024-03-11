8e215eb8b047d9a005ce3b339f331efe6e41fded
Rei Tsukada <rayzou1120@icloud.com>
2024-03-11 21:26:14 +0900

    slackにgptからの返信を表示する

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"github.com/Yokomi422/sample_slack_chatbot/gpt"
 	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/slack-go/slack"
 	"log"
+	"os"
+)
+
+const (
+	CHANNELNAME = "your_channel"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+	SLACKCLIENTTOKEN := os.Getenv("SLACK_CLIENT_SECRET")
+	OpenApiKey := os.Getenv("OPENAI_API_KEY")
+
+	client := openai.NewClient(OpenApiKey)
+	prompt := "sample_message"
+	response, err := gpt.SendPrompt(client, prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := slack.New(SLACKCLIENTTOKEN)
+	_, _, er := c.PostMessage(CHANNELNAME, slack.MsgOptionText(response, true))
+	if er != nil {
+		panic(er)
 	}
 }
