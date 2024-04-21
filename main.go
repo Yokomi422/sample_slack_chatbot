0f3dded66666f74fc254910a6ebdb56761b1332f
Rei Tsukada <rayzou1120@icloud.com>
2024-04-21 23:42:01 +0900

    event処理

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	SLACKCLIENTTOKEN := os.Getenv("SLACK_CLIENT_SECRET")
 	SLACKAPPTOKEN := os.Getenv("SLACK_APP_TOKEN")
 	OpenApiKey := os.Getenv("OPENAI_API_KEY")
@@ -84,8 +85,55 @@ func main() {
 	})
 
 	go func() {
-		log.Println("[INFO] Server listening")
-		http.ListenAndServe(":3000", nil)
+		log.Println("[INFO] Server listening on :3000")
+		if err := http.ListenAndServe(":3000", nil); err != nil {
+			log.Fatal(err)
+		}
 	}()
+
+	go func() {
+		for evt := range socketClient.Events {
+			switch evt.Type {
+			case socketmode.EventTypeConnecting:
+				fmt.Println("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				fmt.Println("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				fmt.Println("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					fmt.Printf("Ignored %+v\n", evt)
+					continue
+				}
+
+				socketClient.Ack(*evt.Request)
+
+				switch eventsAPIEvent.Type {
+				case slackevents.CallbackEvent:
+					innerEvent := eventsAPIEvent.InnerEvent
+					switch ev := innerEvent.Data.(type) {
+					case *slackevents.AppMentionEvent:
+						prompt := strings.TrimSpace(strings.TrimPrefix(ev.Text, fmt.Sprintf("<@%s>", ev.BotID)))
+						response, err := gpt.SendPrompt(client, prompt)
+						if err != nil {
+							log.Printf("Error sending prompt to OpenAI: %v", err)
+							return
+						}
+						_, _, err = api.PostMessage(ev.Channel, slack.MsgOptionText(response, false))
+						if err != nil {
+							log.Printf("Error posting message to Slack: %v", err)
+							return
+						}
+					}
+				default:
+					socketClient.Debugf("Unsupported Events API event received")
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "Unexpected event type received: %s\n", evt.Type)
+			}
+		}
+	}()
+
 	socketClient.Run()
 }
